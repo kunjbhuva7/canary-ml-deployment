85e5af00af20ed90bcd0cf3a39c88e8de1ce7c7e
k-bhuva <kunjbhuva.me@gmail.com>
2025-05-20 16:30:34 +0530

    “2R-Pipline”

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,23 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "19.15.3"
-
   cluster_name    = "ml-eks"
   cluster_version = "1.27"
+  enable_irsa     = true
   vpc_id          = "vpc-0fe1403e8c0a90591"
   subnet_ids      = ["subnet-09b2621a5509c137b", "subnet-0f7876e5059bb0c06"]
 
-  enable_irsa     = true
+  create_kms_key = false
 
   eks_managed_node_groups = {
-    ubuntu_nodes = {
-      instance_types = ["t3a.medium"]
-      desired_size   = 1
-      max_size       = 1
+    default = {
+      name           = "default-node-group"
+      instance_types = ["t2.micro"]
+      desired_size   = 2
+      max_size       = 3
       min_size       = 1
-
-      ami_id         = "ami-0f403e3180720dd7e" # ✅ Ubuntu 20.04 EKS-optimized for us-west-2
+      ami_type       = "AL2_x86_64" # Amazon Linux 2 (Ubuntu not supported directly in managed node groups)
     }
   }
-
-  cluster_enabled_log_types     = ["api", "audit", "authenticator"]
-  create_cloudwatch_log_group   = true
-  create_kms_key                = false
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+}
 }
 
