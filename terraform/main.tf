b139ed54131f9ab30fa147d76ae6e061d30ccc71
k-bhuva <kunjbhuva.me@gmail.com>
2025-05-20 16:54:02 +0530

    “L2-TerraWave”

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "18.29.2"        # ya koi stable latest version
+  version         = "20.36.0"   # latest version
   cluster_name    = "ml-eks"
   cluster_version = "1.27"
   subnet_ids      = ["subnet-09b2621a5509bb0c06", "subnet-09b2621a5509c137b"]
@@ -13,8 +13,7 @@ module "eks" {
     resources = ["secrets"]
   }]
 
-  # Ab node_groups nahi, balki "node_groups" ke jagah "node_group" use karenge:
-  node_group = {
+  node_groups = {
     default = {
       desired_capacity = 2
       max_capacity     = 3
@@ -23,7 +22,9 @@ module "eks" {
     }
   }
 
-  # cluster_logging ke liye:
-  enabled_cluster_log_types = ["api", "audit", "authenticator"]
+  cluster_logging = {
+    enable = true
+    types  = ["api", "audit", "authenticator"]
+  }
 }
 
