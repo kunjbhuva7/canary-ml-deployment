49802f538eb34541561ab8bedb901eb98afd7abf
k-bhuva <kunjbhuva.me@gmail.com>
2025-05-20 17:42:41 +0530

    “DevMesh”

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -11,7 +15,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.36.0"
   cluster_name    = "ml-eks"
-  cluster_version = "1.27"
+  cluster_version = "1.28" # Updated to a supported version (1.27 may be deprecated)
   subnet_ids      = ["subnet-09b2621a5509bb0c06", "subnet-09b2621a5509c137b"]
   vpc_id          = "vpc-0fe1403e8c0a90591"
   enable_irsa     = true
@@ -24,7 +28,7 @@ module "eks" {
       desired_size   = 2
       max_size       = 3
       min_size       = 1
-      instance_types = ["t2.micro"]
+      instance_types = ["t3.medium"] # Updated to a more suitable instance type
     }
   }
   cluster_enabled_log_types = ["api", "audit", "authenticator"]
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  region = "us-east-1" # Updated to match your VPC region
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
+}
