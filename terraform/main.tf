0fb3d2931b0b586c9833d98ffc57f72e340405bc
k-bhuva <kunjbhuva.me@gmail.com>
2025-05-20 16:41:42 +0530

    “M8-Terraform”

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,14 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "ml-eks"
   cluster_version = "1.27"
-  enable_irsa     = true
-  vpc_id          = "vpc-0fe1403e8c0a90591"
   subnet_ids      = ["subnet-09b2621a5509c137b", "subnet-0f7876e5059bb0c06"]
+  vpc_id          = "vpc-0fe1403e8c0a90591"
+  enable_irsa     = true
 
-  create_kms_key = false
+  create_kms_key = true
 
-}  # <-- this closes the module block
+  cluster_encryption_config = [{
+    resources = ["secrets"]
+  }]
+}
 
