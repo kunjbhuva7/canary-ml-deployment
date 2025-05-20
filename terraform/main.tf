6687838e3206fd294056c9a57728beca10022dad
k-bhuva <kunjbhuva.me@gmail.com>
2025-05-20 14:35:02 +0530

    “ML”

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
-module "eks" {
+imodule "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "ml-eks"
   cluster_version = "1.27"
-  subnets         = ["subnet-09b2621a5509c137b", "subnet-0f7876e5059bb0c06"]
+  subnet_ids      =  ["subnet-09b2621a5509c137b", "subnet-0f7876e5059bb0c06"]
   vpc_id          = "vpc-0fe1403e8c0a90591"
   enable_irsa     = true
 }
